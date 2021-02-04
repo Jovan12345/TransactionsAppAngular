83722d7145f9acaeb7988c49142d16c09f05ae58
Jovan12345 <jovelonely1997@gmail.com>
2021-02-04 17:12:36 +0100

    implementing custom pipe, minor styling modifications

--- src/app/shared/pipes/usd-to-diners.pipe.ts
+++ src/app/shared/pipes/usd-to-diners.pipe.ts
@@ -1,18 +1,11 @@
 import { Inject, LOCALE_ID, Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({
-  name: 'usdToDiners'
-})
+@Pipe({name: 'usdToDiners'})
 export class UsdToDinersPipe implements PipeTransform {
 
   constructor(@Inject(LOCALE_ID) protected localeId: string) { }
 
   transform(value: number): number {
-    if (this.localeId === 'sr') {
-      return value * 98;
-    } else {
-      return value
-    }
+    return this.localeId === 'sr' ? value * 98 : value;
   }
-
 }
